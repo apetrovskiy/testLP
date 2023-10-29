4e6f92c0305bd1e48e13683e89d37d7a6f44ca44
Alexander Petrovskiy <uiautomation@hotmail.com>
2023-10-29 16:16:28 +0300

    experimental changes (#11)

--- src/LP.Tests/API/CalculatorResponse.cs
+++ src/LP.Tests/API/CalculatorResponse.cs
@@ -1,4 +1,4 @@
-// <copyright file="CalculatorResponse.cs" company="PlaceholderCompany">
+﻿// <copyright file="CalculatorResponse.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
